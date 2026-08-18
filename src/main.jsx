76aa8eaf032fc55862cb47fbd38fafe0e4be5b36
WorkVolt-tech <ancmarx@gmail.com>
2026-08-17 23:26:13 -0400

    Import CSS styles in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import { AuthProvider } from './contexts/AuthContext.jsx'
+import './styles/index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
