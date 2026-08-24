10e732c129fbb76e01ed8885fde7c9744811c0aa
WorkVolt-tech <ancmarx@gmail.com>
2026-08-24 19:33:39 -0400

    Add TOUR_STEPS for guided tour functionality

---
+++ src/lib/tourSteps.js
@@ -0,0 +1,11 @@
+export const TOUR_STEPS = [
+  { target: null, titleKey: 'tourWelcomeTitle', bodyKey: 'tourWelcomeBody' },
+  { target: 'nav-dashboard', titleKey: 'tourDashboardTitle', bodyKey: 'tourDashboardBody' },
+  { target: 'topbar-location', titleKey: 'tourLocationSwitcherTitle', bodyKey: 'tourLocationSwitcherBody' },
+  { target: 'nav-tables', titleKey: 'tourTablesTitle', bodyKey: 'tourTablesBody' },
+  { target: 'nav-menu', titleKey: 'tourMenuTitle', bodyKey: 'tourMenuBody' },
+  { target: 'nav-staff', titleKey: 'tourStaffTitle', bodyKey: 'tourStaffBody' },
+  { target: 'sidebar-live-requests', titleKey: 'tourLiveRequestsTitle', bodyKey: 'tourLiveRequestsBody' },
+  { target: 'topbar-lang', titleKey: 'tourLangTitle', bodyKey: 'tourLangBody' },
+  { target: 'nav-help', titleKey: 'tourHelpTitle', bodyKey: 'tourHelpBody' },
+]
